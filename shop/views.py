31721274e7c7cd89bdb7fa904390ecf2ea0682df
Amolit <MikeKukPer@gmail.com>
2025-03-06 00:38:15 +0100

    Fix picture for bundles

--- shop/views.py
+++ shop/views.py
@@ -22,6 +22,7 @@ def send_telegram_message(message: str):
         except requests.exceptions.RequestException as e:
             print(f"Error sending message to admin {admin_id}: {e}")
 
+
 class ShopHomeView(TemplateView):
     template_name = 'index.html'
     def get_context_data(self, **kwargs):
@@ -29,28 +30,59 @@ class ShopHomeView(TemplateView):
         context['featured_products'] = Product.objects.filter(available=True).order_by('-created')[:8]
         return context
 
+
 class CatalogView(ListView):
     model = Product
     template_name = 'catalog.html'
     context_object_name = 'products'
     paginate_by = 12
+
     def get_queryset(self):
         queryset = Product.objects.filter(available=True).order_by('-created')
         category_slug = self.request.GET.get('category')
         if category_slug:
             queryset = queryset.filter(category__slug=category_slug)
         return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         context['selected_category'] = self.request.GET.get('category', '')
+
+        # Для кожного товару в списку перевіряємо:
+        # Якщо це бандл (hasattr(product, 'pack_details')) і у нього немає product.image,
+        # шукаємо зображення серед компонентів.
+        for product in context['products']:
+            # Ініціалізуємо product.catalog_image = None, щоб бути впевненими, що поле існує.
+            product.catalog_image = None
+
+            if hasattr(product, 'pack_details') and not product.image:
+                # 1) Шукаємо comp.image
+                for comp in product.pack_details.components.all():
+                    if comp.image:
+                        product.catalog_image = comp.image
+                        break
+
+                # 2) Якщо досі немає, шукаємо перше зображення з comp.images
+                if not product.catalog_image:
+                    for comp in product.pack_details.components.all():
+                        for comp_img_obj in comp.images.all():
+                            if comp_img_obj.image:
+                                product.catalog_image = comp_img_obj.image
+                                break
+                        if product.catalog_image:
+                            break
+
         return context
+
     def get(self, request, *args, **kwargs):
         self.object_list = self.get_queryset()
         context = self.get_context_data()
+        # Якщо запит AJAX, повертаємо лише partial-шаблон із товарами
         if request.headers.get('x-requested-with') == 'XMLHttpRequest':
             return render(request, 'catalog_products.html', context)
         return self.render_to_response(context)
+
 
 class ProductDetailView(DetailView):
     model = Product
@@ -58,26 +90,32 @@ class ProductDetailView(DetailView):
     context_object_name = 'product'
     slug_field = 'slug'
     slug_url_kwarg = 'slug'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        if self.object.is_bundle:
-            # Якщо для бандлу немає окремого зображення, беремо з першого компоненту
+        # Якщо продукт є бандлом
+        if hasattr(self.object, 'pack_details'):
             if self.object.image:
                 main_img = self.object.image
             else:
-                first_comp = self.object.pack_details.components.first()
-                main_img = first_comp.image if first_comp and first_comp.image else None
-            comp_images = []
+                main_img = None
+                for comp in self.object.pack_details.components.all():
+                    if comp.image:
+                        main_img = comp.image
+                        break
+
+            gallery = []
             for comp in self.object.pack_details.components.all():
-                for img in comp.images.all():
-                    if img.image:
-                        comp_images.append(img.image)
-            images = []
-            if main_img:
-                images.append(main_img)
-            images.extend(comp_images)
-            context['images'] = images
+                if comp.image and main_img and comp.image.url != main_img.url:
+                    gallery.append(comp.image)
+                for img_obj in comp.images.all():
+                    if img_obj.image and main_img and img_obj.image.url != main_img.url:
+                        gallery.append(img_obj.image)
+
+            context['main_image'] = main_img
+            context['gallery_images'] = gallery
             context['pack_components'] = self.object.pack_details.components.all()
+            context['images'] = ([main_img] if main_img else []) + gallery
         else:
             if self.object.image:
                 context['images'] = [self.object.image] + list(self.object.images.all())
@@ -85,6 +123,7 @@ class ProductDetailView(DetailView):
                 context['images'] = self.object.images.all()
         context['packs_in'] = self.object.included_in_packs.all()
         return context
+
 
 class CartView(TemplateView):
     template_name = 'cart.html'
@@ -105,6 +144,7 @@ class CartView(TemplateView):
         context['cart_items'] = items
         context['total'] = total
         return context
+
     def post(self, request, *args, **kwargs):
         action = request.POST.get('action')
         product_id = request.POST.get('product_id')
@@ -124,6 +164,7 @@ class CartView(TemplateView):
         request.session['cart'] = cart
         return redirect('shop:cart')
 
+
 class CheckoutView(View):
     template_name = 'checkout.html'
     def get(self, request, *args, **kwargs):
@@ -131,8 +172,9 @@ class CheckoutView(View):
         if not cart:
             return redirect('shop:cart')
         return render(request, self.template_name)
+
     def post(self, request, *args, **kwargs):
-        cart = request.session.get('cart', {})
+        cart = self.request.session.get('cart', {})
         if not cart:
             return redirect('shop:cart')
         full_name = request.POST.get('full_name')
@@ -176,8 +218,10 @@ class CheckoutView(View):
         request.session['cart'] = {}
         return redirect('shop:order_success')
 
+
 class OrderSuccessView(TemplateView):
     template_name = 'order_success.html'
+
 
 class PersonalCabinetView(LoginRequiredMixin, TemplateView):
     template_name = 'personal_cabinet.html'
